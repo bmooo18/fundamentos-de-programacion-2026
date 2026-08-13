d482e2135646d62ffa2fa489d60a4456dff02e4b
Ivan <cvazqacevedo@gmail.com>
2026-08-12 22:26:31 -0600

    Me complique el Hola ivan!

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+// utlizando 3 lineas de codigo
+// muestra el mensaje "Hola <NOMBRE>!"
+
+Console.Write("Hola ");
+Console.Write("Ivan");
+Console.Write("!");
