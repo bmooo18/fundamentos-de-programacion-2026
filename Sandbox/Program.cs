1d1e3e2ca41ee078069bce870b15e8d601c76bf5
Ivan <cvazqacevedo@gmail.com>
2026-08-17 10:50:35 -0600

    Ejercicio 2: Use Colores de fondo y texto en consola para recrear 5 banderas diferentes

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -9,6 +9,8 @@ Console.Write("Hola ");
 Console.Write("Ivan");
 Console.WriteLine("!");
 
+
+
 Console.WriteLine("\n\n\n");
 //Crear un programa que muestre atributos en forma de tabla con un bader
 Console.WriteLine("Estadisticas en forma de tabla");
@@ -18,6 +20,8 @@ Console.WriteLine("/ HP \t100      /");
 Console.WriteLine("\\ MP \t50       \\");
 Console.WriteLine("/ Gold \t2,500    /");
 Console.WriteLine("\\ Exp \t150      \\");
+
+
 
 Console.WriteLine("\n\n\n");
 // Bnadera de Mexico en texto y color
@@ -47,3 +51,159 @@ Console. Write("CO");
 
 Console. ResetColor(); // Reiniciamos los colores
 
+
+
+
+Console.WriteLine("\n\n\n");
+// Generar 5 banderas en texto y 5 en fondo, de 5 paises diferentes
+
+// Bnadera de Francia en texto y color
+Console.WriteLine("Bandera de Francia Texto");
+
+Console. ForegroundColor = ConsoleColor.DarkBlue;
+Console.Write("FR");
+Console. ForegroundColor = ConsoleColor.White;
+Console. Write("ANC");
+Console. ForegroundColor = ConsoleColor.Red;
+Console. Write("IA");
+
+Console. ResetColor(); // Reiniciamos los colores
+Console. WriteLine();  // Salto de línea
+
+Console.WriteLine("Bandera de Francia Fondo");
+
+// Texto en negro para el contraste
+Console. ForegroundColor = ConsoleColor.Black;
+
+Console. BackgroundColor = ConsoleColor. DarkBlue;
+Console. Write("FR");
+Console. BackgroundColor = ConsoleColor. White;
+Console.Write("ANC");
+Console. BackgroundColor = ConsoleColor.Red;
+Console. Write("IA");
+
+Console. ResetColor(); // Reiniciamos los colores
+
+Console.WriteLine("\n\n");
+
+// Bnadera de Belgica en texto y color
+
+Console.WriteLine("Bandera de Belgica Texto");
+
+Console. ForegroundColor = ConsoleColor.Black;
+Console.Write("BE");
+Console. ForegroundColor = ConsoleColor.Yellow;
+Console. Write("LGI");
+Console. ForegroundColor = ConsoleColor.Red;
+Console. Write("CA");
+
+Console. ResetColor(); // Reiniciamos los colores
+Console. WriteLine();  // Salto de línea
+
+Console.WriteLine("Bandera de Belgica Fondo");
+
+// Texto en negro para el contraste
+Console. ForegroundColor = ConsoleColor.White;
+
+Console. BackgroundColor = ConsoleColor. Black;
+Console. Write("BE");
+Console. BackgroundColor = ConsoleColor. Yellow;
+Console.Write("LGI");
+Console. BackgroundColor = ConsoleColor.Red;
+Console. Write("CA");
+
+Console. ResetColor(); // Reiniciamos los colores
+
+Console.WriteLine("\n\n");
+
+// Bnadera de Guatemala en texto y color
+
+Console.WriteLine("Guatemala Texto");
+
+Console. ForegroundColor = ConsoleColor.Blue;
+Console.Write("GUA");
+Console. ForegroundColor = ConsoleColor.White;
+Console. Write("TEM");
+Console. ForegroundColor = ConsoleColor.Blue;
+Console. Write("ALA");
+
+Console. ResetColor(); // Reiniciamos los colores
+Console. WriteLine();  // Salto de línea
+
+Console.WriteLine("Guatemala Fondo");
+
+// Texto en negro para el contraste
+Console. ForegroundColor = ConsoleColor.Black;
+
+Console. BackgroundColor = ConsoleColor. Blue;
+Console. Write("GUA");
+Console. BackgroundColor = ConsoleColor. White;
+Console.Write("TEM");
+Console. BackgroundColor = ConsoleColor.Blue;
+Console. Write("ALA");
+
+Console. ResetColor(); // Reiniciamos los colores
+
+Console.WriteLine("\n\n");
+
+// Bnadera de Guinea en texto y color
+
+Console.WriteLine("Bandera de Guinea Texto");
+
+Console. ForegroundColor = ConsoleColor.Red;
+Console.Write("GU");
+Console. ForegroundColor = ConsoleColor.Yellow;
+Console. Write("IN");
+Console. ForegroundColor = ConsoleColor.DarkGreen;
+Console. Write("EA");
+
+Console. ResetColor(); // Reiniciamos los colores
+Console. WriteLine();  // Salto de línea
+
+Console.WriteLine("Bandera de Guinea Fondo");
+
+// Texto en negro para el contraste
+Console. ForegroundColor = ConsoleColor.Black;
+
+Console. BackgroundColor = ConsoleColor. Red;
+Console. Write("GU");
+Console. BackgroundColor = ConsoleColor. Yellow;
+Console.Write("IN");
+Console. BackgroundColor = ConsoleColor.DarkGreen;
+Console. Write("EA");
+
+Console. ResetColor(); // Reiniciamos los colores
+
+Console.WriteLine("\n\n");
+
+// Bnadera de Peru en texto y color
+
+Console.WriteLine("Bandera de Peru Texto");
+
+Console. ForegroundColor = ConsoleColor.Red;
+Console.Write("P");
+Console. ForegroundColor = ConsoleColor.White;
+Console. Write("ER");
+Console. ForegroundColor = ConsoleColor.Red;
+Console. Write("U");
+
+Console. ResetColor(); // Reiniciamos los colores
+Console. WriteLine();  // Salto de línea
+
+Console.WriteLine("Bandera de Peru Fondo");
+
+// Texto en negro para el contraste
+Console. ForegroundColor = ConsoleColor.Black;
+
+Console. BackgroundColor = ConsoleColor. Red;
+Console. Write("P");
+Console. BackgroundColor = ConsoleColor. White;
+Console.Write("ER");
+Console. BackgroundColor = ConsoleColor.Red;
+Console. Write("U");
+
+Console. ResetColor(); // Reiniciamos los colores
+
+
+
+Console.WriteLine("\n\n\n");
