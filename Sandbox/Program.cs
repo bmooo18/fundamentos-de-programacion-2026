1fd22e5793cd3e96fac443c1a8fea61628894a62
Ivan <cvazqacevedo@gmail.com>
2026-08-17 10:22:55 -0600

    Ejemplo de colore de fondo y texto: Mexico

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -9,7 +9,7 @@ Console.Write("Hola ");
 Console.Write("Ivan");
 Console.WriteLine("!");
 
-Console.WriteLine("n\n\n");
+Console.WriteLine("\n\n\n");
 //Crear un programa que muestre atributos en forma de tabla con un bader
 Console.WriteLine("Estadisticas en forma de tabla");
 Console.WriteLine("/ Greg \"El Mago\" /");
@@ -18,3 +18,32 @@ Console.WriteLine("/ HP \t100      /");
 Console.WriteLine("\\ MP \t50       \\");
 Console.WriteLine("/ Gold \t2,500    /");
 Console.WriteLine("\\ Exp \t150      \\");
+
+Console.WriteLine("\n\n\n");
+// Bnadera de Mexico en texto y color
+Console.WriteLine("Bandera de México Texto");
+
+Console. ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("MÉ");
+Console. ForegroundColor = ConsoleColor.White;
+Console. Write("XI");
+Console. ForegroundColor = ConsoleColor.Red;
+Console. Write("CO");
+
+Console. ResetColor(); // Reiniciamos los colores
+Console. WriteLine(); // Salto de línea
+
+Console.WriteLine("Bandera de México Fondo");
+
+// Texto en negro para el contraste
+Console. ForegroundColor = ConsoleColor.Black;
+
+Console. BackgroundColor = ConsoleColor. DarkGreen;
+Console. Write("MÉ");
+Console. BackgroundColor = ConsoleColor. White;
+Console.Write("XI");
+Console. BackgroundColor = ConsoleColor.Red;
+Console. Write("CO");
+
+Console. ResetColor(); // Reiniciamos los colores
+
